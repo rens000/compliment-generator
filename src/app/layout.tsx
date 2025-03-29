53090770224216215fad5f094bde56d7341d711b
Aria Rens <ariarens@Arias-MacBook-Pro.local>
2025-03-29 02:39:15 -0400

    import font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Sour+Gummy:wght@100..900&display=swap" 
+          rel="stylesheet" 
+        />
+      </head>
       <body
         className={`${geistSans.variable} antialiased`}
       >
